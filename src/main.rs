002a69de85e139f2f3c0236331f32aa5581445d8
Eric Vincent <eric@efvincent.com>
2019-09-06 12:29:11 -0400

    fully implemented get_creds w/ proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 
 #[derive(Debug)]
+/// Credentials for Jira to call the API. For example, the un/pw of a service account.
+/// or if running interactively, the current user
 struct Creds {
     un: String,
     pw: String
 }
 
-fn get_creds() -> Result<Creds,String> {
-    const KEY:&str = "JIRA_CREDS";
-    match std::env::var_os(KEY) {
-        Some (val) => 
-            Ok(Creds { un: format!("{:?}",val), pw: "test".to_string() }),
+/// Returns Jira credentials retrieved from an environment variable
+fn get_creds() -> Result<Creds, String> {
+    let key: &'static str = "JIRA_CREDS";
+    match std::env::var_os(key) {
+        Some (val) => {
+            match val.as_os_str().to_str() {
+                Some (val) => {
+                    let unpw: Vec<&str> = val.rsplit(':').collect();
+                    if unpw.len() != 2 {
+                        Err(format!("Environment key {} should have the format 'un:pw'", key))
+                    } else {
+                        // Note the rsplit function returns the parts in reverse order
+                        Ok(Creds { un: unpw[1].to_string(), pw: unpw[0].to_string() })
+                    }
+                },
+                None => Err(format!("Environment key {} has invalid credentials value. Use the format 'un:pw'", key))
+            }
+        },
         None => 
-            Err(format!("Environment key {} not found", KEY))
+            Err(format!("Environment key {} not found", key))
     }
 }
 
 fn main() {
-    println!("{:?}", get_creds());
+    let creds = get_creds();
+    match creds {
+        Ok(c) => println!("{:?}", c),
+        Err(e) => println!("Error: {}",e)
+    };
 }
