1dc7c6399a631f815b048b41cf610245ed0d0149
Eric Vincent <eric@efvincent.com>
2019-10-04 14:51:21 -0400

    add jira types

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,8 +1,24 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
+open Types.Jira
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let creds = 
+        match Environment.GetEnvironmentVariable("JIRA_CREDS") |> Option.ofObj with
+        | Some s -> 
+            let parts = s.Split(':')
+            if parts.Length = 2 then 
+                Ok (parts.[0], parts.[1]) 
+            else 
+                Error("Invalid format for JIRA_CREDS")
+        | None ->
+            Error("Indefined environment variable JIRA_CREDS")
+
+    match creds with
+    | Ok(un,pw) -> printfn "creds: %s : %s" un pw
+    | Error e -> printfn "Error: %s" e
+
+    let p:Person = { key = "euv0001"; email = "eric@jet.com"; name = "Eric Vincent" } 
+    printfn "person is %s" p.name    
+
+    0 
---
+++ fsharp/types.fs
@@ -0,0 +1,51 @@
+module Types
+open System
+
+    module Jira =
+
+        type Person = {
+            key: string
+            email: string
+            name: string
+        }
+
+        type IssueType =  
+        | Unknown
+        | Issue
+        | Story 
+        | Epic 
+        | Other of string 
+
+        type Component =
+        | Unknown
+        | Mojo
+        | Phoenix
+        | Wolverine 
+        | Ironman
+        | Product
+        | Design
+        | Other of string 
+       
+        type Status = 
+        | Unknown
+        | Backlog
+        | ReadyForWork
+        | Active 
+        | Done 
+        | Deleted 
+        | Other of string 
+
+        type Issue = {
+            key: string
+            id: int
+            summary: string
+            description: string
+            issueType: IssueType
+            points: float
+            components: Component []
+            status: Status
+            resolutionDate: DateTimeOffset
+            created: DateTimeOffset
+            assignee: Person option
+            updated: DateTimeOffset 
+        }
