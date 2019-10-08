e6f1f2a90d8c4f2ea76ec81c59402a8738f5d0e3
Eric Vincent <eric@efvincent.com>
2019-10-08 00:55:46 -0400

    merge existing jira fs work into new structure

--- fsharp/fd-jira/Program.fs
+++ fsharp/fd-jira/Program.fs
@@ -15,13 +15,29 @@ type Stock (ticker, price) =
 
 [<EntryPoint>]
 let main argv =
+
+  let creds = 
+    match Environment.GetEnvironmentVariable("JIRA_CREDS") |> Option.ofObj with
+    | Some s -> 
+        let parts = s.Split(':')
+        if parts.Length = 2 then 
+            Ok (parts.[0], parts.[1]) 
+        else 
+            Error("Invalid format for JIRA_CREDS")
+    | None ->
+        Error("Indefined environment variable JIRA_CREDS")
+
+  match creds with
+  | Ok(un,pw) -> printfn "creds: %s : %s" un pw
+  | Error e -> printfn "Error: %s" e
+
   use db = new LiteDatabase("mydata.db")
   let stocks = db.GetCollection<Stock>()
   let s = Stock("WMT", 108.36)
   s.Id <- 1
   let res = stocks.Upsert s
   printfn "insert result: %A" res
-  s.Comment <- "This is not such a super good stock"
+  s.Comment <- "This is a sample record in the document db"
   match stocks.Update s with 
   | true -> printfn "success!"
   | _ -> printfn "update failed?"
