b2b51f9f7a009fb5d7109ce177c1018225665f96
Eric Vincent <eric@efvincent.com>
2019-10-09 03:03:49 -0400

    added fn for http call to query jira

--- fsharp/fd-jira/Program.fs
+++ fsharp/fd-jira/Program.fs
@@ -2,6 +2,13 @@
 open System.IO
 open LiteDB
 open System.Text.Json
+open System.Net.Http
+
+// use one HttpClient for all calls
+let httpClient = new HttpClient()
+
+[<Literal>]
+let BASE_URL = "https://jira.walmart.com/rest/api/2"
 
 type Stock (ticker, price) = 
   member val Id = 0 with get,set
@@ -11,24 +18,36 @@ type Stock (ticker, price) =
   override this.ToString () =
     sprintf "[%i] %s @ %f : %s" this.Id this.Ticker this.Price this.Comment
 
-[<EntryPoint>]    
-let main argv =
+let makeJiraCall (creds:string) url =
+  async {
+    let req = new HttpRequestMessage()
+    req.Method <- HttpMethod.Get
+    req.RequestUri <- new Uri(url)  
+    req.Headers.Authorization <-
+      let bytes = System.Text.UTF8Encoding.UTF8.GetBytes creds
+      let encodedCreds = System.Convert.ToBase64String bytes
+      new Headers.AuthenticationHeaderValue("Basic", encodedCreds)
+    let! rsp = httpClient.SendAsync req |> Async.AwaitTask
+    if rsp.IsSuccessStatusCode then 
+      return! rsp.Content.ReadAsStringAsync() |> Async.AwaitTask
+    else 
+      return ""
+  }
 
-  let creds = 
-    match Environment.GetEnvironmentVariable("JIRA_CREDS") |> Option.ofObj with
-    | Some s -> 
-        let parts = s.Split(':')
-        if parts.Length = 2 then 
-            Ok (parts.[0], parts.[1]) 
-        else 
-            Error("Invalid format for JIRA_CREDS")
-    | None ->
-        Error("Indefined environment variable JIRA_CREDS")
+let makeQuery project (updatedSince:DateTimeOffset) =
+  let dt = updatedSince.ToString("yyyy-MM-dd HH:mm")
+  let query = sprintf "project=%s AND updatedDate >= \"%s\"" project dt
+  Uri.EscapeDataString query
+  
+let getChangedIssues creds baseUrl startAt =
+  async {
+    let query = makeQuery "RCTFD" DateTimeOffset.MinValue
+    let url = sprintf "%s/search?jql=%s&expand=name&maxResults=100&fields=updated&startAt=%i"
+                baseUrl query startAt
+    return! makeJiraCall creds url
+  }
 
-  match creds with
-  | Ok(un,pw) -> printfn "creds: %s : %s" un pw
-  | Error e -> printfn "Error: %s" e
-
+let doDbStuff () =
   use db = new LiteDatabase("mydata.db")
   let stocks = db.GetCollection<Stock>()
   let s = Stock("WMT", 108.36)
@@ -44,4 +63,16 @@ let main argv =
   let results = stocks.Find(fun x -> x.Comment.StartsWith("This"))
   printfn "%A" results
 
+
+[<EntryPoint>]    
+let main argv =
+
+  let creds = 
+    match Environment.GetEnvironmentVariable("JIRA_CREDS") |> Option.ofObj with
+    | Some s -> s
+    | None -> ""
+
+  let result = getChangedIssues creds BASE_URL 0 |> Async.RunSynchronously
+  printfn "result:\n%s" result
+  
   0
