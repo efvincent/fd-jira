49b4bcdc6a9ade054bee87f698fdff78956ec4d8
Eric Vincent <eric@efvincent.com>
2019-10-09 22:24:48 -0400

    System.Text.Json F# helper fns

---
+++ fsharp/fd-jira/jsonParsing.fs
@@ -0,0 +1,50 @@
+module efvJson 
+open System
+open System.Text.Json
+
+  type JsonParseError =
+  | Unknown
+  | TypeError of string * JsonElement
+  | PropNotFound of string * JsonElement
+
+  with
+    override this.ToString() =
+      match this with
+      | Unknown -> "Unknown Parse Error"
+      | TypeError(desiredType,_) -> desiredType
+      | PropNotFound(pname, _) -> sprintf "Property %s not found" pname
+
+  exception JsonParseExn of JsonParseError
+
+  let private _typeErr jvk desiredType je =
+    let msg = sprintf "Count not parse (%s) as %s" (string jvk) desiredType
+    TypeError(msg, je)
+
+  let getInt (je:JsonElement) =
+    match je.TryGetInt32 () with
+    | (true, n) ->  n
+    | (false, _) -> 
+      raise <| JsonParseExn(_typeErr je.ValueKind "Integer" je)
+
+  let getStr (je:JsonElement) =
+    match je.ValueKind with 
+    | JsonValueKind.String -> je.GetString()
+    | jvk -> 
+      raise <| JsonParseExn(_typeErr jvk "String" je)
+
+  let getStrOpt (je:JsonElement) =
+    match je.ValueKind with
+    | JsonValueKind.String -> je.GetString() |> Some
+    | JsonValueKind.Null -> None
+    | jvk -> 
+      raise <| JsonParseExn(_typeErr jvk "String Option" je)
+
+  let getProp (n:string) (je:JsonElement) =
+    match je.TryGetProperty n with
+    | (true, je') -> je'
+    | (false, _)  -> raise <| JsonParseExn(PropNotFound(n, je))
+
+  let getPropInt n = (getProp n) >> getInt 
+  let getPropStr n = (getProp n) >> getStr
+  let getPropStrOpt n = (getProp n) >> getStrOpt
+  
