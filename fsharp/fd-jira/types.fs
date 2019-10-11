dfddf95c588933d1cfc84698fe39d962a18f2685
Eric Vincent <eric@efvincent.com>
2019-10-11 18:07:13 -0400

    continue to extend Issue type

--- fsharp/fd-jira/types.fs
+++ fsharp/fd-jira/types.fs
@@ -1,67 +1,75 @@
 module Types
 open System
 
-    module Jira =
+  module Jira =
 
-        type Person = {
-            key: string
-            email: string
-            name: string
-        }
+    type Person = {
+      key: string
+      email: string
+      name: string
+    }
 
-        type IssueType =  
-        | Unknown
-        | Issue
-        | Story 
-        | Epic 
-        | Other of string 
+    [<RequireQualifiedAccess>]
+    type IssueType =  
+    | Unknown
+    | Issue
+    | Story 
+    | Epic 
+    | SubTask
+    | Other of string 
 
-        type Component =
-        | Unknown
-        | Mojo
-        | Phoenix
-        | Wolverine 
-        | Ironman
-        | Product
-        | Design
-        | Other of string 
-       
-        type Status = 
-        | Unknown
-        | Backlog
-        | ReadyForWork
-        | Active 
-        | Done 
-        | Deleted 
-        | Other of string 
+    type Component =
+    | Unknown
+    | Mojo
+    | Phoenix
+    | Wolverine 
+    | Ironman
+    | Product
+    | Design
+    | Other of string 
+   
+    type Status = 
+    | Unknown
+    | Backlog
+    | ReadyForWork
+    | Active 
+    | Done 
+    | Deleted 
+    | Other of string 
 
-        type Issue = {
-            key           : string
-            id            : int
-            summary       : string
-            description   : string
-            issueType     : IssueType
-            points        : float
-            components    : Component []
-            status        : Status
-            resolutionDate: DateTimeOffset
-            created       : DateTimeOffset
-            assignee      : Person option
-            updated       : DateTimeOffset
-        }
+    type Issue2 = {
+      key           : string
+      id            : int
+      summary       : string
+      description   : string
+      issueType     : IssueType
+      points        : float
+      components    : Component []
+      status        : Status
+      resolutionDate: DateTimeOffset
+      created       : DateTimeOffset
+      assignee      : Person option
+      updated       : DateTimeOffset
+    }
 
-        type TestIssue = {
-            key        : string
-            id         : string
-            summary    : string
-            description: string option
-            link       : string
-            points     : float option
-        }
-        with
-            override this.ToString() =
-                sprintf "(%s) %s\nsummary: %s\ndescription: %s\nlink: %s\npoints: %f" 
-                        this.id this.key this.summary 
-                        (this.description |> Option.defaultValue "[none]")
-                        this.link
-                        (this.points |> Option.defaultValue 0.)
+    type Issue = {
+      key        : string
+      id         : string
+      summary    : string
+      description: string option
+      issueType  : IssueType
+      status     : Status
+      components : Component seq
+      link       : string
+      points     : float option
+    }
+    with
+      override this.ToString() =
+        sprintf "%s %s [%s]\nstatus: %s\nsummary: %s\nlink: %s\npoints: %f" 
+                this.key
+                (string this.issueType)
+                (this.components |> Seq.map string |> (fun comps -> String.Join(",", comps)))
+                (string this.status) this.summary 
+                this.link
+                (this.points |> Option.defaultValue 0.)
+
