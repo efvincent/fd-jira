9a1f5484d481b7d91c813f5893ad66c4afff984c
Eric Vincent <eric@efvincent.com>
2019-10-17 02:11:11 -0400

    break up random blobs into cleaner fns

--- fsharp/fd-jira/Program.fs
+++ fsharp/fd-jira/Program.fs
@@ -20,36 +20,50 @@ let getUpdatedItems creds =
       printfn "Error: %s" e
   }
 
+let jsonSerializerOptions =
+  let jo = JsonSerializerOptions()
+  jo.WriteIndented <- true
+  jo
+
+let div = String('-', 80)
+
+let jsonToStr (jd:JsonDocument) = JsonSerializer.Serialize(jd.RootElement, jsonSerializerOptions)
+  
 let getIssue creds issue =
   async {
     match! JiraApi.getIssue creds BASE_URL issue with
     | Ok jd ->
-      // let rs = 
-      //   let opts = JsonSerializerOptions()
-      //   opts.WriteIndented <- true
-      //   JsonSerializer.Serialize(jd.RootElement, opts)
-      // printfn "\nIssue:\n%s\n" rs 
+      printfn "\nIssue:\n%s\n" (jsonToStr jd) 
       return Issue.fromJson jd.RootElement
     | Error e ->
       printfn "Error: %s" e
       return Error e
   }
 
+let printIssues creds startAt count =
+  [1..count] 
+  |> List.map(fun n -> sprintf "RCTFD-%i" (startAt + n))
+  |> List.iter(fun id -> 
+    match getIssue creds id |> Async.RunSynchronously with
+    | Ok issue -> printfn "%s\nIssue: %s" div (string issue)
+    | Error e -> printfn "%s" e     
+  )
+
+let printFields creds =
+  match JiraApi.getFields creds BASE_URL |> Async.RunSynchronously with
+  | Ok jd ->
+    printfn "%s\nfields:\n%s" div (jsonToStr jd)
+  | Error e -> printfn "%s" e
+
 [<EntryPoint>]    
 let main argv =
   printfn "FD-Jira. Experiments in Jira API driven utility.\nCopyright 2019-2020 Eric F. Vincent\n"
-  let div = String('-', 80)
   Environment.GetEnvironmentVariable("JIRA_CREDS")
   |> Result.ofObj "No Creds Found!"
   |> Result.map (fun cr ->
-      [1..20] 
-      |> List.map(fun n -> sprintf "RCTFD-%i" (3840 + n))
-      |> List.iter(fun id -> 
-        match getIssue cr id |> Async.RunSynchronously with
-        | Ok issue -> printfn "%s\nIssue: %s" div (string issue)
-        | Error e -> printfn "%s" e     
-      )
+      printIssues cr 4100 5
     )
   |> ignore
+
 
   0
