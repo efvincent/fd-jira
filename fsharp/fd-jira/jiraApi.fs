33dcf351505cc419ad5ea467931f70ed0e85194e
Eric Vincent <eric@efvincent.com>
2019-10-17 02:10:16 -0400

    method to get field defs

--- fsharp/fd-jira/jiraApi.fs
+++ fsharp/fd-jira/jiraApi.fs
@@ -43,8 +43,14 @@ let getChangedIssues creds baseUrl startAt =
   }
 
 let getIssue creds baseUrl issue =
-    async {
-        let url = sprintf "%s/issue/%s?fields=assignee,status,summary,description,created,updated,resolutiondate,issuetype,components,priority,resolution,customfield_10002"
-                            baseUrl issue
-        return! makeJiraCall creds url
-    }
+  async {
+    let url = sprintf "%s/issue/%s?fields=assignee,status,summary,description,created,updated,resolutiondate,issuetype,components,priority,resolution,customfield_10002,subtasks,customfield_10007,parent"
+                        baseUrl issue
+    return! makeJiraCall creds url
+  } 
+
+let getFields creds baseUrl =
+  async {
+    let url = sprintf "%s/field" baseUrl
+    return! makeJiraCall creds url
+  }
