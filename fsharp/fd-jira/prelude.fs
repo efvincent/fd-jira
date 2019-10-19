2acb9236487e140fdfb595f2ff639930b970ce36
Eric Vincent <eric@efvincent.com>
2019-10-18 20:11:59 -0400

    add prelude file

---
+++ fsharp/fd-jira/prelude.fs
@@ -0,0 +1,29 @@
+module Prelude
+
+  open Serilog
+  open Microsoft.Extensions.Configuration
+  open System
+
+  type Ctx = {
+    log  : ILogger
+    creds: string
+    cId  : Guid
+  } 
+  with 
+    member this.SetCreds creds = { this with creds = creds }
+
+  let initCtx =
+    let config = 
+      ConfigurationBuilder()
+        .AddJsonFile("config.json")
+        .Build()
+    let log = 
+      LoggerConfiguration()
+        .ReadFrom.Configuration(config)
+        .CreateLogger() :> ILogger    
+    log.Information "Logger initialized"
+    {
+      log = log
+      creds = ""
+      cId = Guid.NewGuid()
+    }
