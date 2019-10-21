b4d8ccd6c4d180a2ab5a9043856ab5483d14d14a
Eric Vincent <eric@efvincent.com>
2019-10-21 19:00:10 -0400

    save issue to db

--- fsharp/fd-jira/database.fs
+++ fsharp/fd-jira/database.fs
@@ -1,26 +1,19 @@
-module DataBase
+module Database
+
 open LiteDB
+open LiteDB.FSharp
+open Types.Jira
 
-type Stock (ticker, price) = 
-  member val Id = 0 with get,set
-  member val Ticker = ticker with get,set
-  member val Price = price with get,set
-  member val Comment = "" with get,set
-  override this.ToString () =
-    sprintf "[%i] %s @ %f : %s" this.Id this.Ticker this.Price this.Comment
+let mapper = FSharpBsonMapper()
+let db = new LiteDatabase("Filename=test.db;Mode=Exclusive", mapper)
 
-let doDbStuff () =
-  use db = new LiteDatabase("mydata.db")
-  let stocks = db.GetCollection<Stock>()
-  let s = Stock("WMT", 108.36)
-  s.Id <- 1
-  let res = stocks.Upsert s
-  printfn "insert result: %A" res
-  s.Comment <- "This is a sample record in the document db"
-  match stocks.Update s with 
-  | true -> printfn "success!"
-  | _ -> printfn "update failed?"
-
-  stocks.EnsureIndex(fun x -> x.Comment) |> ignore
-  let results = stocks.Find(fun x -> x.Comment.StartsWith("This"))
-  printfn "%A" results
+let saveIssue (ctx:Prelude.Ctx) (issue:Issue) =
+  try 
+    ctx.log.Debug("Database.saveIssue|start|issue.id:{id}", issue.id)
+    let items = db.GetCollection<Issue>("issues")
+    items.Insert(issue) |> ignore
+    ctx.log.Debug("Database.saveIssue|end|issue.id:{id}", issue.id)
+    Ok ()
+  with ex ->
+    ctx.log.Error("Database.saveIssue|issue.id:{id}|{errorMessage}", issue.id, ex.Message)
+    Error (string ex)
