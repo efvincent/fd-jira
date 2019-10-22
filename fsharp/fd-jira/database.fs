c307103e5ec6110d8d80e2809cbe259122e5e958
Eric Vincent <eric@efvincent.com>
2019-10-22 18:48:04 -0400

    initial work on db for saving/getting issues and project info

--- fsharp/fd-jira/database.fs
+++ fsharp/fd-jira/database.fs
@@ -1,19 +1,50 @@
 module Database
 
+open System
 open LiteDB
 open LiteDB.FSharp
+open LiteDB.FSharp.Extensions
 open Types.Jira
 
 let mapper = FSharpBsonMapper()
 let db = new LiteDatabase("Filename=test.db;Mode=Exclusive", mapper)
+let issues = db.GetCollection<Issue>("issues")
+let systemState = 
+  let col = db.GetCollection<SystemState>("sysState")
+  col.EnsureIndex(fun s -> s.project) |> ignore
+  col
+
+let saveSystemState (ctx:Prelude.Ctx) (project:string) (lastUpdate:DateTimeOffset) =
+  ctx.log.Debug("Database.saveSystemState|start|project:{project}|lastUpdate:{lastUpdate}",
+    project, lastUpdate)
+
+let tryGetSystemState (ctx:Prelude.Ctx) (project:string) =
+  ctx.log.Debug("Database.getSystemState|start|project:{project}", project)
+  match systemState.tryFindOne<SystemState> <@ fun ss -> ss.project = project @> with
+  | Some ss -> 
+    ctx.log.Debug("Database.getSystemState|end|project:{project}", project)
+    Some ss
+  | None -> 
+    ctx.log.Information("Database.getSystemState|not-found|project:{project}", project)
+    None
 
 let saveIssue (ctx:Prelude.Ctx) (issue:Issue) =
   try 
     ctx.log.Debug("Database.saveIssue|start|issue.id:{id}", issue.id)
-    let items = db.GetCollection<Issue>("issues")
-    items.Insert(issue) |> ignore
+    issues.Upsert(issue) |> ignore
     ctx.log.Debug("Database.saveIssue|end|issue.id:{id}", issue.id)
     Ok ()
   with ex ->
     ctx.log.Error("Database.saveIssue|issue.id:{id}|{errorMessage}", issue.id, ex.Message)
     Error (string ex)
+
+let tryGetIssue (ctx:Prelude.Ctx) (key:string) =
+  ctx.log.Debug("Database.getIssue|start|key:{key}", key)
+  match issues.tryFindOne<Issue> <@ fun issue -> issue.key = key @> with
+  | Some issue -> 
+    ctx.log.Debug("Database.getIssue|end|key:{key}", key)
+    Some issue
+  | None ->
+    ctx.log.Error("Database.getIssue|not-found|key:{key}", key)
+    None
+  
