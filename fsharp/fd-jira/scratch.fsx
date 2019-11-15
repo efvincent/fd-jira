96a0f496f4c093d1a5c8307c5cbc15e08c792912
Eric Vincent <eric@efvincent.com>
2019-11-15 09:56:18 -0500

    start work on the year parser for dates (eventually)

--- fsharp/fd-jira/scratch.fsx
+++ fsharp/fd-jira/scratch.fsx
@@ -1,5 +1,5 @@
-#r @"/Users/eric.vincent/.nuget/packages/fparsec/1.0.4-rc3/lib/netstandard1.6/FParsecCS.dll"
-#r @"/Users/eric.vincent/.nuget/packages/fparsec/1.0.4-rc3/lib/netstandard1.6/FParsec.dll"
+#r @"/Users/euv0001/.nuget/packages/fparsec/1.0.4-rc3/lib/netstandard1.6/FParsecCS.dll"
+#r @"/Users/euv0001/.nuget/packages/fparsec/1.0.4-rc3/lib/netstandard1.6/FParsec.dll"
 
 open FParsec
 
@@ -8,6 +8,6 @@ let test s p =
   | Success(v,_,_) -> printfn "Success: %A" v
   | Failure(e,_,_) -> printfn "Failure: %s" e
 
-let pyear:Parser<string,unit> = many1Satisfy isDigit
+let pyear:Parser<string,unit> =  pipe4 digit digit digit digit (fun c1 c2 c3 c4 -> sprintf "%c%c%c%c" c1 c2 c3 c4)
 
-
+test pyear "1234"
