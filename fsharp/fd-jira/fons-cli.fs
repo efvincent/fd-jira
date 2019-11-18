3d9f7ab9ebeeb9e1e128dbb1c83037719d0f21be
Eric Vincent <eric@efvincent.com>
2019-11-18 17:46:30 -0500

    separate out styles ala css sort of

--- fsharp/fd-jira/fons-cli.fs
+++ fsharp/fd-jira/fons-cli.fs
@@ -2,16 +2,27 @@ module FonsCli
 open System
 open Fons.Components
 
+let private style = 
+  {|
+    sysName     = [fg 100 100 250; bold]
+    projectCode = [fg 255 150 120; bold]
+    delimiter   = [fg 220 220 20]
+    data        = [fg 250 100 100]
+    plain       = [fg 100 100 100] 
+    keyword     = [fg 150 150 250]
+  |}
+
   let getCmdLinePrompt () =
     let curTime = DateTime.Now
     block
       [
         space
-        text [fg 0 0 255] "FlightDeck"
-        text [fg 220 220 20] "["
-        text [fg 250 100 100] (sprintf "%02i:%02i:%02i" curTime.Hour curTime.Minute curTime.Second)
-        text [fg 220 220 20] "]"
+      text style.sysName "Jira "
+      text style.projectCode "RCTFD "
+      text style.delimiter "["
+      text style.data (sprintf "%02i:%02i:%02i" curTime.Hour curTime.Minute curTime.Second)
+      text style.delimiter "]"
         space
-        text [fg 100 100 100] "$"
+      text style.plain "$"
         space
       ]
