08e88776cd0b839e4a01085c57885bee15e55adc
Eric Vincent <eric@efvincent.com>
2019-11-22 23:52:41 -0500

    initial documentation of jsonParsing module

--- fsharp/fd-jira/jsonParsing.fs
+++ fsharp/fd-jira/jsonParsing.fs
@@ -1,3 +1,4 @@
+/// Wrap the .NET Core 3 JsonParsing in functions for more F Sharpiness
 module Json 
 
 open System
@@ -23,18 +24,22 @@ open System.Text.Json
     let msg = sprintf "Count not parse (%s) as %s" (string jvk) desiredType
     TypeError(msg, je)
 
+  /// Extract integer value from a JsonElement. Throws if value is not an integer
   let getInt (je:JsonElement) =
     match je.TryGetInt32 () with
     | (true, n) ->  n
     | (false, _) -> 
       raise <| JsonParseException(_typeErr je.ValueKind "Integer" je)
 
+  /// Extract string value from JsonElement. Throws if value is not a string
   let getStr (je:JsonElement) =
     match je.ValueKind with 
     | JsonValueKind.String -> je.GetString()
     | jvk -> 
       raise <| JsonParseException(_typeErr jvk "String" je)
 
+  /// Extract a string option from a JsonElement. If null value will be None, if
+  /// a string value, it will be Some value. Otherwise it throws.
   let getStrOpt (je:JsonElement) =
     match je.ValueKind with
     | JsonValueKind.String -> je.GetString() |> Some
@@ -42,17 +47,22 @@ open System.Text.Json
     | jvk -> 
       raise <| JsonParseException(_typeErr jvk "String Option" je)
 
+  /// Extract float option from JsonElement. If null value will be None, if
+  /// a number, Some value will be returned. Throws otherwise.
   let getFloatOpt (je:JsonElement) =
     match je.ValueKind with
     | JsonValueKind.Number -> je.GetDouble() |> Some
     | JsonValueKind.Null -> None
     | jvk -> raise <| JsonParseException(_typeErr jvk "float option" je)
 
+  /// Extract a float option from JsonElement. Throws if value is not a number
   let getFloat je =
     match getFloatOpt je with
     | Some f -> f
     | None -> raise <| JsonParseException(_typeErr je.ValueKind "float" je)
 
+  /// Extracts a DateTimeOffset option from a JsonElement. If value is null, None is
+  /// returned. If a date value can be extracted, it will be Some value. Throws otherwise.
   let getDTOOpt (je:JsonElement) =
     match je.ValueKind with
     | JsonValueKind.String -> 
